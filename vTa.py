b76367e87c02a719a9c89719ab2a685ebb7865c7
renren82 <xianghui_ren@126.com>
2020-01-29 15:02:08 +0800

    comment start date

--- vTa.py
+++ vTa.py
@@ -5,12 +5,12 @@ import pandas as pd
 from numpy import *
 import numpy as np
 
-ts_code_str = '000559.SZ'
-st_code_str = '000559'
+ts_code_str = '002403.SZ'
+st_code_str = '002403'
 
-dt_start = datetime.datetime.strptime('20181009', '%Y%m%d').date()
+dt_start = datetime.datetime.strptime('20181225', '%Y%m%d').date()
 dt_start_str = dt_start.strftime('%Y%m%d')
-dt_end = datetime.datetime.strptime('20190131', '%Y%m%d').date()
+dt_end = datetime.datetime.strptime('20200123', '%Y%m%d').date()
 dt_end_str = dt_end.strftime('%Y%m%d')
 
 path_root = 'H:/'
@@ -20,6 +20,8 @@ path_result = path_root + ts_code_str + '_result.xlsx'
 dt_now = datetime.datetime.now().date()
 # dt_now = datetime.datetime.strptime('20160101', '%Y%m%d').date()
 dt_now_str = dt_now.strftime('%Y%m%d')
+dt_data_start = dt_now - datetime.timedelta(days=800)
+dt_data_start_str = dt_data_start.strftime('%Y%m%d')
 
 
 def file_exist(path):
@@ -34,7 +36,7 @@ def get_ta_data(path_data):
     get base delta value for ma3-ma5
     """
     if file_exist(path_data) == 0:
-        df = ts.pro_bar(ts_code=ts_code_str, adj='qfq', start_date=dt_start_str,
+        df = ts.pro_bar(ts_code=ts_code_str, adj='qfq', start_date=dt_data_start_str,
                         end_date=dt_now_str, ma=[3, 5, 8, 13, 21, 34, 55, 89, 144, 233])
         # print(df.head())
         writer = pd.ExcelWriter(path_data)
@@ -63,12 +65,17 @@ def v_ta(file_path):
             high_price_list.append(df_data.loc[k, 'high'])
             low_price_list.append(df_data.loc[k, 'low'])
             price_date_list.append(df_data.loc[k, 'trade_date'])
-
+        if df_data.loc[k, 'trade_date'] == dt_start_str:
+            break
+    if flag_find == 0:
+        return 0
     low_price = float(min(low_price_list))
     high_price = float(max(high_price_list))
     high_price_date_str = price_date_list[high_price_list.index(high_price)]
 
-    if (high_price - low_price)*100/low_price < 25:
+    bigger = (high_price - low_price)*100/low_price
+    print(bigger)
+    if bigger < 25:
         return 0
 
     # bigger 25% is v
@@ -78,8 +85,12 @@ def v_ta(file_path):
     for k in df_data.index.values:
         # print(type(df_data.loc[k, 'trade_date']))
         if df_data.loc[k, 'trade_date'] == high_price_date_str:
-            for i in range(0, k):
+            i = 0
+            while 1:
+                if df_data.loc[k+i, 'trade_date'] == dt_start_str:
+                    break
                 vol_up += df_data.loc[k, 'vol']
+                i += 1
             while 1:
                 if df_data.loc[k, 'trade_date'] == dt_end_str:
                     break
@@ -89,7 +100,8 @@ def v_ta(file_path):
                 vol_down += df_data.loc[k, 'vol']
 
                 # price is lower
-                if (high_price - v_low_price)*100/v_low_price > 15:
+                lower = (high_price - v_low_price)*100/v_low_price
+                if lower > 15:
                     # is lower vol
                     if vol_up/vol_down > 1.0:
                         flag_lower = 0
@@ -100,6 +112,8 @@ def v_ta(file_path):
                             if v_close_price > df_data.loc[k, ma_str]:
                                 flag_lower = 1
                         if flag_lower == 0:
+                            # print(lower)
+                            # print(vol_up / vol_down)
                             print(df_data.loc[k, 'trade_date'])
 
 
