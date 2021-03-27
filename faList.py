be98783750288be181cecb24795fd39423baafcb
ren <xianghui_ren@126.com>
2021-03-27 10:29:10 +0800

    comment 20210206

--- faList.py
+++ faList.py
@@ -22,7 +22,29 @@ dt_start_str = dt_start.strftime('%Y%m%d')
 
 pro = ts.pro_api( )
 
-#  SSE SZSE
+# 输入参数
+#
+# 名称	类型	必选	描述
+# is_hs	str	N	是否沪深港通标的，N否 H沪股通 S深股通
+# list_status	str	N	上市状态： L上市 D退市 P暂停上市
+# exchange	str	N	交易所 SSE上交所 SZSE深交所 HKEX港交所(未上线)
+# 输出参数
+#
+# 名称	类型	描述
+# ts_code	str	TS代码
+# symbol	str	股票代码
+# name	str	股票名称
+# area	str	所在地域
+# industry	str	所属行业
+# fullname	str	股票全称
+# enname	str	英文全称
+# market	str	市场类型 （主板/中小板/创业板）
+# exchange	str	交易所代码
+# curr_type	str	交易货币
+# list_status	str	上市状态： L上市 D退市 P暂停上市
+# list_date	str	上市日期
+# delist_date	str	退市日期
+# is_hs	str	是否沪深港通标的，N否 H沪股通 S深股通
 data = pro.stock_basic(exchange='SSE', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
 # print(type(data))
 writer = pd.ExcelWriter(filedownloadpath + 'list.xlsx')
