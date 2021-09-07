982ee14339fcd13a98aaf7b05e9b398c71482450
ren <xianghui_ren@126.com>
2021-09-07 08:34:28 +0800

    comment 20210907

--- mp4tomp3.py
+++ mp4tomp3.py
@@ -9,4 +9,4 @@ for file_name in list_file:
     # 要转换的mp4文件
     video = VideoFileClip(path_root + file_name)
     audio = video.audio
-    audio.write_audiofile(path_out + file_name[:-4] + '.mp3')
+    audio.write_audiofile(path_out + file_name[:-4] + '.mp3', bitrate="320k")
